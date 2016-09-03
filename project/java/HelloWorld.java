22a3a26cbeb807fc3f89ddf0a64460733d94095a
kaito.higa <kaito.higa@inte.co.jp>
2016-09-04 08:07:31 +0900

    初回コミット

---
+++ project/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Print of hello world.
+ * 
+ * @author kaito.higa
+ * @since 2016.09.04
+ */
+public class HelloWorld {
+	public static final void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
